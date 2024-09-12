0b8812b659d862c46364725a0f9db55b3252c01e
Esteban Abaroa <estebanabaroa@protonmail.com>
2024-09-12 18:09:25 +0000

    add cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import { fileURLToPath } from 'url'
 import indexRouter from './routes/index.js'
 import providersRouter from './routes/providers.js'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
+import cors from 'cors'
 
 const app = express()
 
@@ -20,6 +21,7 @@ app.use((req, res, next) => {
 // trust x-forwarded-for, the app will almost always use a proxu
 app.set('trust proxy', true)
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json({
   limit: '1mb',
